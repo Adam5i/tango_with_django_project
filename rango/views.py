f0db5843f22fb1903f020b38a641233773394071
Adam5i <czyzewski.i.a@gmail.com>
2018-01-24 08:42:33 +0000

    Through 6th chapter

--- rango/views.py
+++ rango/views.py
@@ -2,12 +2,15 @@
 #from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rango.models import Category
 
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
